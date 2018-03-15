fdcac6da1682cf692bbe006690c5d274e21d0479
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-03-14 19:58:35 -0600

    Add static folder for cname file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => (
       <h1 className="responsive-headline">Hello, I'm Jimmy Cleveland.</h1>
       <h3>
         I do <span>frontend development</span>, with a preference for{' '}
-        <span>Javascript</span>, <span>Node</span>, and <span>React</span>.
+        <span>Javascript</span>, <span>Node.js</span>, and <span>React.js</span>.
       </h3>
     </div>
   </div>
