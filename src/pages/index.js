bdef8bae960f19549b08b1e663d38d713b72a8c8
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-04-20 10:22:49 -0600

    Remove 'Testimonials' component and slick carousel packages/css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import Header from '../components/Header';
 import About from '../components/About';
 import Resume from '../components/Resume';
 import Portfolio from '../components/Portfolio';
-import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
 
 const IndexPage = () => (
