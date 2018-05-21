53bcf846a3ca245cb9bc11d4907188ae213277d8
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-05-20 18:00:30 -0600

    Updates

--- path---shopify-cross-sells-app-a0e39f21c11f6a62c5ab.js
+++ path---shopify-cross-sells-app-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xe28587a12a44],{971:function(t,a){t.exports={pathContext:{}}}});
+webpackJsonp([0xe28587a12a44],{972:function(t,a){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---shopify-cross-sells-app-a0e39f21c11f6a62c5ab.js.map
