6b678c15fddf38f125fc1616a505df0949d8470f
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-06-01 13:12:05 -0600

    Add Open Graph meta-data

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { siteMetadata } from '../../gatsby-config';
 import favicon from './../assets/favicon.ico';
+import unfurl from './../assets/images/jimmydc_unfurl.jpg';
 import lazySizes from 'lazysizes';
 
 const TemplateWrapper = ({ children }) => (
@@ -10,6 +11,13 @@ const TemplateWrapper = ({ children }) => (
     <Helmet>
       <meta name="description" content="Jimmy Cleveland's Portfolio Site" />
       <meta name="author" description={siteMetadata.author} />
+      <meta
+        property="og:title"
+        content="Jimmy Cleveland's Developer Portfolio"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://jimmydc.com/" />
+      <meta property="og:image" content={unfurl} />
       <title>{siteMetadata.title}</title>
       <link rel="icon" href={favicon} type="image/x-icon" />
     </Helmet>
